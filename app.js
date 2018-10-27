25d200234ac10259cd5c66b3c2ec3a5030ba1da6
Cheyra <staiercm@gmail.com>
2018-10-27 10:34:18 -0400

    1stjs

---
+++ app.js
@@ -0,0 +1,10 @@
+$(document).ready(){
+$("#submit").on("click", function(){
+    $("#name").val().trim()
+$("#role").val().trim()
+$("#startDate").val().trim()
+$("#salary").val().trim()
+
+})
+
+})
